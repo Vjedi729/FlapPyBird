a79f400715c63f9689c4c84e4d50b5c8e9037b3e
Rick Sheahan <rssheah2@illinois.edu>
2016-09-28 18:28:44 -0500

    Fix weirdness i made in getrandompipe

--- flappy.py
+++ flappy.py
@@ -355,7 +355,7 @@ def getRandomPipe():
     gapY = random.randrange(0, int(BASEY * 0.6 - PIPEGAPSIZE))
     gapY += int(BASEY * 0.2)
     pipeHeight = IMAGES['pipe'][0].get_height()
-    pipeX = SCREENWIDTH + 200
+    pipeX = SCREENWIDTH + 10
 
     return [
         {'x': pipeX, 'y': gapY - pipeHeight},  # upper pipe
